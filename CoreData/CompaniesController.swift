2fa93ac25464f4c84ddab612b443da23d76740d1
Don <mcallisterda@gmail.com>
2019-09-16 16:04:54 -0400

    Introduce model object for dynamic list and create company modal, shifted navigation styling to app delegate using appearance proxy

--- CoreData/CompaniesController.swift
+++ CoreData/CompaniesController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class CompaniesController: UITableViewController {
+    
+    let companies = [
+        Company(name: "Apple", founded: Date()),
+        Company(name: "Google", founded: Date()),
+        Company(name: "Facebook", founded: Date())
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +38,14 @@ class ViewController: UITableViewController {
     
     @objc func handleAddCompany() {
         print("Adding company...")
+        
+        let createCompanyController = CreateCompanyController()
+       // createCompanyController.view.backgroundColor = .green
+        
+        let navController = CustomNavigationController(rootViewController: createCompanyController)
+        
+        present(navController, animated: true, completion: nil)
+        
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -46,7 +60,10 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "The Company Name"
+        
+        let company = companies[indexPath.row]
+        
+        cell.textLabel?.text = company.name
         cell.textLabel?.textColor = .white
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         
@@ -55,19 +72,11 @@ class ViewController: UITableViewController {
         
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8
+        return companies.count
     }
     
     func setupNavigationStyle() {
-        navigationController?.navigationBar.isTranslucent = false
-        
-       
-        navigationController?.navigationBar.barTintColor = .lightRed
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        //make white when scrolling up:
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        //moved to AppDelegate in appearance proxy
     }
 
 }
---
+++ CoreData/CreateCompanyController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateCompanyController.swift
+//  CoreData
+//
+//  Created by Donald McAllister on 9/16/19.
+//  Copyright © 2019 Donald McAllister. All rights reserved.
+//
+
+import UIKit
+
+class CreateCompanyController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Create Company"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        view.backgroundColor = .darkBlue
+        
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
